97bc6265bd21ae14718bffd2cbc4ac4f2505c8f3
Tristan Lins <tristan.lins@infinitysoft.de>
2012-07-15 20:18:45 +0200

    Rename extension to follow new PSR-0 poc.

---
+++ system/modules/Namespaces/config/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package Namespaces
+ * @link    http://www.contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'ce_namespace_module'  => 'system/modules/namespaces/templates',
+	'mod_namespace_module' => 'system/modules/namespaces/templates',
+));
